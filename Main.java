1a258c1273ef8571915093a477df2e653989a8d2
Judekiing <judekiingokorie@gmail.com>
2023-05-30 11:12:46 +0100

    first commit

---
+++ Main.java
@@ -0,0 +1,25 @@
+public class Main {
+  public static void main(String[] args) {
+
+// First Java script    
+System.out.println("Hello World");
+
+//Tell us who you are: 
+    System.out.println("I am a Jude Okorie, a Java Hero");
+
+//Want do you want people to remeber about you: 
+    System.out.println("I want to build the next big application that changes the world");
+
+//How do you plan to achieve this?
+    System.out.print("I want to learn to write codes. I store my code super powers  on github");
+
+/* I need to learn to solve complex maths in splits seconds. My IT Instruction knows how to write different progragramming languages. I would stay quiet and listen to all his instructions and follow them accordindly */
+
+    System.out.print("I trust my IT Instructor to teach me the ways of Hero past ");
+
+/* Time to Hone our Mathematical skills! 
+Addition */
+    System.out.println(691);
+     System.out.println(30 * 8);
+  }
+}
