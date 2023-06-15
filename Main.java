ce0000517d2a25c87ad8e7ddb0e230f09f065b07
Judekiing <judekiingokorie@gmail.com>
2023-06-15 10:50:38 +0100

    created first variable

--- Main.java
+++ Main.java
@@ -1,25 +1,17 @@
 public class Main {
-  public static void main(String[] args) {
+    public static void main(String [] arg) {
 
-// First Java script    
-System.out.println("Hello World");
+// command to out put "Hello World!"
+     System.out.println("Hello World");
 
-//Tell us who you are: 
-    System.out.println("I am a Jude Okorie, a Java Hero");
 
-//Want do you want people to remeber about you: 
-    System.out.println("I want to build the next big application that changes the world");
+     System.out.println("Class work");
+     System.out.println("My Holiday was awesome");
+     System.out.println("I developed a program to create customers Invoice, awesome right!");
+       System.out.print("It feels like having 1000 SUPER POWERS!");
 
-//How do you plan to achieve this?
-    System.out.print("I want to learn to write codes. I store my code super powers  on github");
-
-/* I need to learn to solve complex maths in splits seconds. My IT Instruction knows how to write different progragramming languages. I would stay quiet and listen to all his instructions and follow them accordindly */
-
-    System.out.print("I trust my IT Instructor to teach me the ways of Hero past ");
-
-/* Time to Hone our Mathematical skills! 
-Addition */
-    System.out.println(691);
-     System.out.println(30 * 8);
-  }
+  System.out.println("We arrived at 1000 by multiplying ");
+ System.out.print( "20 * 50 = ");
+ System.out.println( 20 * 50);
+    }
 }
---
+++ Variable.java
@@ -0,0 +1,11 @@
+public class Variable {
+  public static void main(String[] args) {
+
+String Name = "J. Okorie";
+int Age = 370;
+String occupation = "Dev";
+
+
+    System.out.println("My Name is: $Name");
+  }
+}
