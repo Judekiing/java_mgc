0a0a11f42257ac7c6d3762cf542e3aecf5e2fd5e
Judekiing <judekiingokorie@gmail.com>
2023-06-19 12:56:49 +0100

    Tested calculator

--- Calculator.java
+++ Calculator.java
@@ -7,15 +7,24 @@ class Calculator {
 	// Here we declare variables for our calculator 
 	String name;
 	char operator;
-	int number1, number2;
+	double number1, number2;
 	double result;
 
 	//create an object of scanner class
 	Scanner input = new Scanner(System.in);
+	
+	while (true) {
 
 	//At this point we want to request user to choose an operator 
 	System.out.println("Choose an operator: +, -, *, or /");
+	System.out.println("Enter E to exit");
+
 	operator = input.next().charAt(0);
+	if (operator == 'E'){
+	System.out.println("Thank you for using our Calculator! See you soon.");
+	
+		break;
+	}
 
 	//ask users to enter numbers
 	System.out.println("Enter first value");
@@ -42,8 +51,8 @@ class Calculator {
 
 	//performs multiplication between numbers
 	// checking if the operstor is "*"
-	if (operator == '*') {
-		result = number1 * number2;
+	else if (operator == '*') {
+		result = number1*number2;
 		System.out.println(number1 + "*" + number2 + "=" +result);
 		
 	}
@@ -51,9 +60,15 @@ class Calculator {
 	//performs division between numbers
 	// checking if the operstor is "/"
 	else if (operator == '/') {
-		result = number1 / number2;
+		result =  number1 / number2;
 		System.out.println(number1 + "/" + number2 + "=" +result);
 		
+	}
+	
+	//condition to display incorrect operator 
+	else {
+		System.out.println("Kindly confirm the operator you choose exist");
+	}
 	}
 
 	 
